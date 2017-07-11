2ddf3a6ff5a8d121841e041dc0466c0acf272535
mitsunorinaraoka <mitsunori.naraoka@ipg.co.jp>
2017-07-11 11:53:50 +0900

    coverageを追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "ggm_domain"
 require "timecop"
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start do
+  add_filter '/vendor/'
+end
